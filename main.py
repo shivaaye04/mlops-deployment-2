46012de37c6c8f872cdba387e517ebe5ec22a5cb
shivaaye <chinmayachaturvedi@gmail.com>
2026-07-03 20:47:04 +0530

    fix mlflow model path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 app = FastAPI()
 
-model = mlflow.pyfunc.load_model("mlartifacts")
+model = mlflow.pyfunc.load_model("mlartifacts/0/models/m-8b217a65e6c0426bb9ddd83fbda5e258")
 
 @app.get("/")
 def home():
